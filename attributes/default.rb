f1f912d369320177208e7a249a6b573846178b8f
vancawa <wannes@cloudar.be>
2017-01-26 14:29:02 +0100

    added stuff

---
+++ attributes/default.rb
@@ -0,0 +1,11 @@
+default['jboss']['zip_installer'] = 'http://download.jboss.org/jbossas/7.1/jboss-as-7.1.1.Final/jboss-as-7.1.1.Final.zip'
+default['jboss']['home'] = '/opt/jboss-as'
+default['jboss']['installation_type'] = 'standalone'
+default['jboss']['uid'] = 110
+default['jboss']['gid'] = 110
+default['jboss']['ear_files'] = []
+
+### configure the java cookbook to use oracle java 8
+default['java']['jdk_version'] = '8'
+default['java']['oracle']['accept_oracle_download_terms'] = true
+default['java']['install_flavor'] = 'oracle'
---
+++ resources/ear.rb
@@ -0,0 +1,41 @@
+resource_name :ear
+property :file, kind_of: String, name_property: true
+
+load_current_value do
+  if ::File.exists?("#{node['jboss']['home']}/#{node['jboss']['installation_type']}/deployments/#{file}")
+    if ! Dir["#{node['jboss']['home']}/#{node['jboss']['installation_type']}/deployments/#{file}.*"].empty?
+      Dir["#{node['jboss']['home']}/#{node['jboss']['installation_type']}/deployments/#{file}.*"].first.split('.').last
+    else
+      return 'not_deployed'
+    end
+  else
+    return 'not_deployed'
+  end
+end
+
+action :deploy do
+  puts "Deploying #{::File.basename(file)}"
+  file ::File.basename("#{node['jboss']['home']}/#{node['jboss']['installation_type']}/#{file}") do
+    owner 'jboss'
+    group 'jboss'
+    mode '0600'
+    content lazy { ::File.open(file).read }
+  end
+  while Dir("#{node['jboss']['home']}/#{node['jboss']['installation_type']}/#{file}.deployed").empty?
+    status = Dir("#{node['jboss']['home']}/#{node['jboss']['installation_type']}/#{file}.*").first.split['.'].last
+    puts status
+    raise if status == 'failed'
+    sleep 5
+  end
+end
+
+action :undeploy do
+  puts "Undeploying #{::File.basename(file)}"
+  file file do
+    :delete
+  end
+  while Dir("#{node['jboss']['home']}/#{node['jboss']['installation_type']}/#{file}.undeployed").empty?
+    puts 'Undeploying'
+    sleep 5
+  end
+end
