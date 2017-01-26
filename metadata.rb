5089b86d840a3410a8a8b5bef8a76be1d338126b
vancawa <wannes@cloudar.be>
2017-01-26 13:29:30 +0100

    added chef boilerplate

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'jboss'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures jboss'
+long_description 'Installs/Configures jboss'
+version          '0.1.0'
