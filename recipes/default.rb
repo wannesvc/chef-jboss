be93960ab45e36bd8337b4bd9cb6b8fde3b9054f
vancawa <wannes@cloudar.be>
2017-02-09 15:03:34 +0100

    dont run anything on cookbook inclusion

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,13 +1,13 @@
 #jboss_install node['jboss']['version']
 
-node['jboss']['ear_files'].each do |ear|
-  jboss_ear ear.split('/').last do
-    source_path ear 
-    deploy_path "#{node['jboss']['home']}/#{node['jboss']['installation_type']}/deployments"
-  end
-end
-node['jboss']['users'].each do |user|
-  jboss_user user[:username] do
-    password user[:password]
-  end
-end
+#node['jboss']['ear_files'].each do |ear|
+#  jboss_ear ear.split('/').last do
+#    source_path ear 
+#    deploy_path "#{node['jboss']['home']}/#{node['jboss']['installation_type']}/deployments"
+#  end
+#end
+#node['jboss']['users'].each do |user|
+#  jboss_user user[:username] do
+#    password user[:password]
+#  end
+#end
