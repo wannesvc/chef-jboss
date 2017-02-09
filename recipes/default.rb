a50157da310e18535948c8732dd6c1719db5084c
vancawa <wannes@cloudar.be>
2017-02-09 16:35:38 +0100

    removed test

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,11 +1,11 @@
-jboss_install node['jboss']['version']
+#jboss_install node['jboss']['version']
 
-node['jboss']['ear_files'].each do |ear|
-  jboss_ear ear.split('/').last do
-    source_path ear 
-    deploy_path "#{node['jboss']['home']}/#{node['jboss']['installation_type']}/deployments"
-  end
-end
+#node['jboss']['ear_files'].each do |ear|
+#  jboss_ear ear.split('/').last do
+#    source_path ear 
+#    deploy_path "#{node['jboss']['home']}/#{node['jboss']['installation_type']}/deployments"
+#  end
+#end
 #node['jboss']['users'].each do |user|
 #  jboss_user user[:username] do
 #    password user[:password]
